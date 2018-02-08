b6243d89e072ad79365a281c6b1e47f99fcd9ec6
Shilpa Sawant test <shilpa.sawant@cts.local>
2018-02-08 12:42:52 +0530

    ckeditor initial commit

---
+++ ckeditor/custom-config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	config.uiColor = '#ffffff';
+	config.height = '400px';
+	//added on 28th sep
+	//lite.userName = document.getElementById("users").value;
+	
+};
---
+++ ckeditor/samples/new.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--
+Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+-->
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CKEditor Sample</title>
+	<script src="../ckeditor.js"></script>
+	<script src="js/sample.js"></script>
+	<link rel="stylesheet" href="css/samples.css">
+	<link rel="stylesheet" href="toolbarconfigurator/lib/codemirror/neo.css">
+</head>
+<body id="main">
+
+
+dfsgfdhgfdhj
+
+
+<main>
+	
+	<div class="adjoined-bottom">
+		<div class="grid-container">
+			<div class="grid-width-100">
+				<textarea name="editor1" class="ckeditor"></textarea>
+   <script type="text/javascript">
+      CKEDITOR.replace( 'editor1' );
+      CKEDITOR.add            
+   </script>
+
+
+			</div>
+		</div>
+	</div>
+
+</main>
+
+
+
+</body>
+</html>
